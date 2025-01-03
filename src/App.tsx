14302e5d2364b8e3128ff0744fd42de08be6c1d6
Oleg Kiryushin <fewgwer3@yandex.ru>
2025-01-04 00:40:50 +0300

    feat: implement react-router, add layout, base pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Route, Routes } from 'react-router';
+import Layout from './components/Layout/Layout';
+import { AnotherPage, HomePage, NotFoundPage } from './pages';
 import './App.css';
 
 function App() {
-  return <>App</>;
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path={'/another-page'} element={<AnotherPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/pages/index.ts
@@ -0,0 +1,5 @@
+import AnotherPage from './AnotherPage';
+import HomePage from './HomePage';
+import NotFoundPage from './NotFoundPage';
+
+export { AnotherPage, HomePage, NotFoundPage };
